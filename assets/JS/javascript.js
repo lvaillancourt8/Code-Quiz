831a138addb53c85bf03d68143d6855258876e07
lvaillan <classvaillan@gmail.com>
2021-07-11 10:18:35 -0500

    added stop timer feature and final score notifications

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,54 +1,103 @@
 var timerEl = document.querySelector("#timer");
 var timerBoxEl = document.querySelector('#timer-box');
-var secondsLeft = 60;
+var timerInterval;
+var timer_is_on = false;
+var secondsLeft = 30;
 var startQuizButtonEl = document.querySelector("#start-quiz");
 var question = 0;
 var questionCorrect = 0;
 var questionWrong = 0;
 var welcomeEL = document.querySelector("#welcome");
 var question1El = document.querySelector("#question1");
-var questionSection = document.querySelector("#question-section");
+var questionSectionEL = document.querySelector("#question-section");
 var element;
-var finalScoreReveal = document.querySelector("#final-score");
+var finalScoreEl = document.querySelector("#final-score");
+var validScoreEL = document.querySelector("#valid-score");
+var noScoreEL = document.querySelector("#no-score");
+var leaderScoreEl = document.querySelector(".leader-score");
+var scoreEL = document.querySelector("#score");
 
-function setTimer() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
+//Quiz Timer Functions
+//Timer Interval
+function timedCount() {
+  timerInterval = setInterval(function() {
     timerEl.textContent = secondsLeft;
+    secondsLeft--;
     if(secondsLeft <= 5 && secondsLeft >= 1) {
       timerBoxEl.setAttribute("style", "color: red");
     }
     else if(secondsLeft === 0) {
       clearInterval(timerInterval);
       timerBoxEl.innerHTML = "Time's Up!";
+      questionSectionEL.classList.add("hidden");
+      finalScoreEl.classList.remove("hidden");
+      noScoreEL.classList.remove("hidden");
     }
   }, 1000);
 }
 
+//Start Countdown
+function startCount() {
+  if (!timer_is_on) {
+    timer_is_on = true;
+    timedCount();
+  }
+}
+
+//Stop Countdown
+function stopCount() {
+  clearTimeout(timerInterval);
+  timer_is_on = false;
+}
+
+// //Reset. Counter
+// function resetCount() {
+//   stopCount();
+//   c = 0;
+//   document.getElementById("textbox").value = c;
+//     }
+
+// function setTimer() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timerEl.textContent = secondsLeft;
+//     if(secondsLeft <= 5 && secondsLeft >= 1) {
+//       timerBoxEl.setAttribute("style", "color: red");
+//     }
+//     else if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       timerBoxEl.innerHTML = "Time's Up!";
+//     }
+//   }, 1000);
+// }
+
 function showNextQuestion() {
   element.parentElement.classList.add("hidden");
   var parent = element.parentElement;
-    if(parent.nextElementSibling !== "null") {
+    if(parent.nextElementSibling !== null) {
       parent.nextElementSibling.classList.remove("hidden");
     }
   else {
-    // stop the clock
     finalScore();
   }
 }
 
 function finalScore() {
-  finalScoreReveal.classList.remove("hidden");
+  stopCount();
+  scoreEL.textContent = secondsLeft;
+  timerBoxEl.innerHTML = "Great Job!"
+  finalScoreEl.classList.remove("hidden");  
+  validScoreEL.classList.remove("hidden");
 }
 
 startQuizButtonEl.addEventListener("click", function(){
-  setTimer();
+  startCount();
   welcomeEL.setAttribute("class", "hidden");
   question1El.classList.remove("hidden");
 });
 
 // Listen for any clicks within quiz question container
-questionSection.addEventListener("click", function(event) {
+questionSectionEL.addEventListener("click", function(event) {
   element = event.target;
 
   // Check if the clicked element was a button
