fb58242046d1fac6257fd1198cd8ab7184f6b093
lvaillan <classvaillan@gmail.com>
2021-07-11 14:16:54 -0500

    fixed bugs and included functionality for storing high scores in local storage

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -2,7 +2,7 @@ var timerEl = document.querySelector("#timer");
 var timerBoxEl = document.querySelector('#timer-box');
 var timerInterval;
 var timer_is_on = false;
-var secondsLeft = 30;
+var secondsLeft = 60;
 var startQuizButtonEl = document.querySelector("#start-quiz");
 var question = 0;
 var questionCorrect = 0;
@@ -14,8 +14,16 @@ var element;
 var finalScoreEl = document.querySelector("#final-score");
 var validScoreEL = document.querySelector("#valid-score");
 var noScoreEL = document.querySelector("#no-score");
-var leaderScoreEl = document.querySelector(".leader-score");
+var leaderScoreNoticeEl = document.querySelector("#leader-score-notice");
+var leaderScoreInputEl = document.querySelector("#leader-score-input");
 var scoreEL = document.querySelector("#score");
+var enterInitialsBtnEL = document.querySelector("#enter-initials-btn");
+var initialsEL = document.querySelector("#initials");
+var topScores = JSON.parse(localStorage.getItem("topScores")) || [];
+var leaderBoardEl = document.querySelector("#leader-board");
+var scoreList = document.querySelector("#score-list");
+var thanksEl = document.querySelector("#thanks");
+var viewLeaderBoardBtnEl = document.querySelector("#view-leader-board");
 
 //Quiz Timer Functions
 //Timer Interval
@@ -50,27 +58,6 @@ function stopCount() {
   timer_is_on = false;
 }
 
-// //Reset. Counter
-// function resetCount() {
-//   stopCount();
-//   c = 0;
-//   document.getElementById("textbox").value = c;
-//     }
-
-// function setTimer() {
-//   var timerInterval = setInterval(function() {
-//     secondsLeft--;
-//     timerEl.textContent = secondsLeft;
-//     if(secondsLeft <= 5 && secondsLeft >= 1) {
-//       timerBoxEl.setAttribute("style", "color: red");
-//     }
-//     else if(secondsLeft === 0) {
-//       clearInterval(timerInterval);
-//       timerBoxEl.innerHTML = "Time's Up!";
-//     }
-//   }, 1000);
-// }
-
 function showNextQuestion() {
   element.parentElement.classList.add("hidden");
   var parent = element.parentElement;
@@ -84,10 +71,34 @@ function showNextQuestion() {
 
 function finalScore() {
   stopCount();
-  scoreEL.textContent = secondsLeft;
-  timerBoxEl.innerHTML = "Great Job!"
-  finalScoreEl.classList.remove("hidden");  
-  validScoreEL.classList.remove("hidden");
+  if (secondsLeft > 0) {
+    scoreEL.textContent = secondsLeft;
+    timerBoxEl.innerHTML = "Great Job!"
+    finalScoreEl.classList.remove("hidden");  
+    validScoreEL.classList.remove("hidden");
+  }
+  else {
+    timerBoxEl.innerHTML = "Time's Up!";
+    finalScoreEl.classList.remove("hidden");  
+    noScoreEL.classList.remove("hidden");
+  }
+}
+
+function storeUserScore(event) {
+  event.preventDefault();
+  var score = {
+    "score": secondsLeft,
+    "initials": initialsEL.value
+  }
+  topScores.push(score);
+  localStorage.setItem("topScores", JSON.stringify(topScores));
+  finalScoreEl.setAttribute("class", "hidden");
+  thanksEl.classList.remove("hidden");
+}
+
+function showLeaderBoard(event) {
+  // document.body.getElementsByTagName("section").classList.add("hidden");
+  leaderBoardEl.classList.remove("hidden");
 }
 
 startQuizButtonEl.addEventListener("click", function(){
@@ -99,7 +110,6 @@ startQuizButtonEl.addEventListener("click", function(){
 // Listen for any clicks within quiz question container
 questionSectionEL.addEventListener("click", function(event) {
   element = event.target;
-
   // Check if the clicked element was a button
   if (element.matches("button")) {
     var correct = element.getAttribute("class");
@@ -113,3 +123,6 @@ questionSectionEL.addEventListener("click", function(event) {
   showNextQuestion();
 });
 
+enterInitialsBtnEL.addEventListener("click", storeUserScore);
+
+viewLeaderBoardBtnEl.addEventListener("click", showLeaderBoard);
